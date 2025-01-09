20d7be7b66933289d3faa1bcb930f5484bd78a42
11 <120713858+KWJ02@users.noreply.github.com>
2025-01-09 15:15:41 +0900

    exe 빌드 설정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      mainProcessFile: 'src/background.js',
+      builderOptions: {
+        appId: 'talkwave.app',
+        productName: 'TalkWave',
+        win: {
+          icon: 'build/icon.ico',
+          target: [
+            {
+              target: 'nsis',
+              arch: ['x64']
+            }
+          ]
+        },
+        nsis: {
+          oneClick: false,
+          allowToChangeInstallationDirectory: true,
+          createDesktopShortcut: true
+        },
+        files: [
+          "**/*",
+          "dist/**/*"
+        ],
+        extraResources: ['./dist/**']
+      }
+    }
+  }
+}
